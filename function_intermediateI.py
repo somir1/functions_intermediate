5b40c78862b3cd13256cf90982e8f5fe66a33457
somir1 <leonsomir@gmail.com>
2021-07-07 17:31:46 -0500

    Part 2 update

--- function_intermediateI.py
+++ function_intermediateI.py
@@ -1,4 +1,4 @@
-
+#Part 1
 # x = [ [5,2,3], [10,8,9] ] 
 # students = [
 #      {'first_name':  'Michael', 'last_name' : 'Jordan'},
@@ -25,24 +25,27 @@
 # z[0]['y'] = 30
 # print(z[0])
 
-# for index, item in enumerate(items):
-#     print(index, item)
-# def iterateDictionary(students):
-#     for index, student in enumerate(students):
-#         tempIdx = index;
-#         for key, value in student.items():
-#             print(key + '-' + value)
 
-# students = [
-#         {'first_name':  'Michael', 'last_name' : 'Jordan'},
-#         {'first_name' : 'John', 'last_name' : 'Rosales'},
-#         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
-#         {'first_name' : 'KB', 'last_name' : 'Tonel'}
-# ]
+#Part 2
+students = [
 
-# iterateDictionary(students)
-# should output: (it's okay if each key-value pair ends up on 2 separate lines;
-# bonus to get them to appear exactly as below!)
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+
+
+def iterateDictionary(list):
+    for i in range(0, len(list)):
+        output = ""
+        for key, val in list[i].items():
+            output += f"{key} - {val},"
+        print(output)
+
+iterateDictionary(students)
+
+#part 3
 
 # def iterateDictionary2(key_name, some_list):
 #     for student in some_list:
@@ -58,15 +61,17 @@
 # iterateDictionary2('first_name', students)
 # iterateDictionary2('last_name', students)
 
-dojo = {
-   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
-   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
-}
+#part 4
 
-def printInfo(some_dict):
-    for key, value in some_dict.items():
-        print(f"{len(value)} {key}")
-        for i in range(0, len(value)):
-            print(value[i])
-printInfo(dojo)
+# dojo = {
+#    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+#    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+# }
+
+# def printInfo(some_dict):
+#     for key, value in some_dict.items():
+#         print(f"{len(value)} {key}")
+#         for i in range(0, len(value)):
+#             print(value[i])
+# printInfo(dojo)
 
